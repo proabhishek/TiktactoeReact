bfd661f28c1d897070c074f239c502f1d4ba47a9
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2021-07-14 19:54:02 +0530

    tic toc toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Icon from "./Components/Icon"
+// import react-toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import reactstrap
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import "./style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const tiktacArray = new Array(9).fill("")
+const App = () => {
+    let [isCross, setIsCross] = useState(true)
+    let [winMessage, setWinMessage] = useState("")
+
+    const playAgain=()=>{
+        setIsCross(true)
+        setWinMessage("")
+        tiktacArray.fill("")
+    }
+
+    const findWinner=()=>{
+       
+    }
+
+ 
+
+    return(
+        <div> <Icon choice="sdvkbhf"/> </div>
+    )
 }
 
-export default App;
+export default App
+
